c11126c2ccf716c0928c48cbc1a38754dddecd63
Yash Auti <yashauti2471@gmail.com>
2024-09-05 20:57:00 +0530

    change uri

--- frontend/src/apiConfig.js
+++ frontend/src/apiConfig.js
@@ -5,7 +5,7 @@ if(import.meta.env.VITE_NODE_ENV==="production"){
     baseUrl="deployed-url";
     //socketUrl="wss;//deployedurl"
 }else{
-    baseUrl="http://localhost:4000";
+    baseUrl="https://design-with-yash.onrender.com";
     //socketUrl="ws://localhost:4000";
 }
 
